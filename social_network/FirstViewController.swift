835e8ce0291417f06dac6e41b9602c54dd4accdf
Radiksidenko <radik-sidehnko@ukr.net>
2018-03-12 17:47:00 +0200

    Initial Commit

---
+++ social_network/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  social_network
+//
+//  Created by Radomyr Sidenko on 12.03.2018.
+//  Copyright © 2018 Radomyr Sidenko. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ social_network/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  social_network
+//
+//  Created by Radomyr Sidenko on 12.03.2018.
+//  Copyright © 2018 Radomyr Sidenko. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
