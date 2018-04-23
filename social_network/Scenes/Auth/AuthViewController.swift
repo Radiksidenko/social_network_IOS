0f0fe955d7c540e562d812a924a200a676ab4d23
Radiksidenko <radik-sidehnko@ukr.net>
2018-04-23 18:06:29 +0300

    cleaned the code

--- social_network/Scenes/Auth/AuthViewController.swift
+++ social_network/Scenes/Auth/AuthViewController.swift
@@ -11,6 +11,55 @@ import FirebaseAuth
 
 class AuthViewController: UIViewController{
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ////////////////////////background/////////////////////
+        //self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg img")!)
+        
+        let background = UIImage(named: "bg img")
+        var imageView : UIImageView!
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode =  UIViewContentMode.scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        imageView.center = view.center
+        view.addSubview(imageView)
+        self.view.sendSubview(toBack: imageView)
+        ////////////////////////////////////////////
+        
+        ///////////////Button//////////////////
+        self.buttonLogin?.layer.borderWidth = 1.0
+        let borderAlpha : CGFloat = 0.7
+        self.buttonLogin?.layer.borderColor = UIColor(white: 1.0, alpha: borderAlpha).cgColor
+        /////////////////////////////////
+        
+        ///////////////textField//////////////////
+        let border = CALayer()
+        let border2 = CALayer()
+        let width = CGFloat(2.0)
+        border.borderColor = UIColor.darkGray.cgColor
+        border.borderWidth = width
+        border2.borderColor = UIColor.darkGray.cgColor
+        border2.borderWidth = width
+        
+        ///////////////input//////////////////
+        if((username) != nil){
+            border.frame = CGRect(x: 0, y: (username?.frame.size.height)! - width, width:  (username.frame.size.width), height: (username?.frame.size.height)!)
+            username?.layer.addSublayer(border)
+            username?.layer.masksToBounds = true
+        }
+        
+        ///////////////password input//////////////////
+        if((password) != nil){
+            border2.frame = CGRect(x: 0, y: password.frame.size.height - width, width:  password.frame.size.width, height: password.frame.size.height)
+            password.layer.addSublayer(border2)
+            password.layer.masksToBounds = true
+        }
+        /////////////////////////////////
+        
+    }
+    
+    ////////////////////////Spinner/////////////////////
     class func displaySpinner(onView : UIView) -> UIView {
         let spinnerView = UIView.init(frame: onView.bounds)
         spinnerView.backgroundColor = UIColor.init(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.5)
@@ -31,16 +80,34 @@ class AuthViewController: UIViewController{
             spinner.removeFromSuperview()
         }
     }
+    /////////////////////////////////////////////
+    
+    //////////////////switc page////////////////////////
+    private func switcPage(page: String){
+        guard let appDeligate = UIApplication.shared.delegate as? AppDelegate else {return}
+        guard let main = UIStoryboard(name: page, bundle: Bundle.main).instantiateInitialViewController() else {return}
+        appDeligate.window?.rootViewController = main
+        
+    }
+    //////////////////////////////////////////
     
     
+    ////////////////////////window alert/////////////////////
+    func displayAlert(userMessage: String) {
+        var myAlert = UIAlertController(title: "Alert", message: userMessage, preferredStyle: UIAlertControllerStyle.alert)
+        let ok = UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil)
+        myAlert.addAction(ok)
+        self.present(myAlert, animated: true, completion: nil)
+    }
+    /////////////////////////////////////////////
     
     @IBOutlet weak var username: UITextField!
     @IBOutlet weak var password: UITextField!
-    
     @IBOutlet weak var buttonLogin: UIButton!
     
-    
+    ////////////////////////Login/////////////////////
     @IBAction func login(_ sender: Any) {
+        
         let sv = AuthViewController.displaySpinner(onView: self.view)
         guard let usernameText = username.text,
             let passwordText = password.text
@@ -49,77 +116,24 @@ class AuthViewController: UIViewController{
         Auth.auth().signIn( withEmail: usernameText,
                             password: passwordText){[weak self] (user, error) in
                                 if let error = error{
-                                    debugPrint("$$$$$$$$$$$$$$$")
                                     debugPrint(error.localizedDescription)
                                     debugPrint("$$$$$$$$$$$$$$$")
-                                    debugPrint("œœœœœœœœœœœœœœ")
+                                    debugPrint("login")
+                                    debugPrint("$$$$$$$$$$$$$$$")
                                     AuthViewController.removeSpinner(spinner: sv)
-                                    
-                                    let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
-                                    alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
-                                        NSLog("The \"OK\" alert occured.")
-                                    }))
-                                    
-                                    self?.present((alert), animated: true, completion: nil)
-                                    
+                                    self?.displayAlert(userMessage: error.localizedDescription)
                                 }
-                                
                                 
                                 if !(error != nil){
                                     AuthViewController.removeSpinner(spinner: sv)
-                                    debugPrint("////////////")
-                                    debugPrint(user)
-                                    self?.switcMain()
+                                    self?.switcPage(page: "Main")
                                 }
                                 
         }
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-//        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg img")!)
-        /////////////////////////
-        let background = UIImage(named: "bg img")
-        
-        var imageView : UIImageView!
-        imageView = UIImageView(frame: view.bounds)
-        imageView.contentMode =  UIViewContentMode.scaleAspectFill
-        imageView.clipsToBounds = true
-        imageView.image = background
-        imageView.center = view.center
-        view.addSubview(imageView)
-        self.view.sendSubview(toBack: imageView)
-        /////////////////////////
-        ///////////////loginButton//////////////////
-        self.buttonLogin?.layer.borderWidth = 1.0
-        let borderAlpha : CGFloat = 0.7
-        self.buttonLogin?.layer.borderColor = UIColor(white: 1.0, alpha: borderAlpha).cgColor
-        ///////////////End loginButton//////////////////
-        ///////////////textField//////////////////
-        let border = CALayer()
-        let border2 = CALayer()
-        let width = CGFloat(2.0)
-        border.borderColor = UIColor.darkGray.cgColor
-        border.borderWidth = width
-        
-        border2.borderColor = UIColor.darkGray.cgColor
-        border2.borderWidth = width
-        ///////////////username input//////////////////
-        if((username) != nil){
-            border.frame = CGRect(x: 0, y: (username?.frame.size.height)! - width, width:  (username.frame.size.width), height: (username?.frame.size.height)!)
-            username?.layer.addSublayer(border)
-            username?.layer.masksToBounds = true
-        }
-        
-        ///////////////password input//////////////////
-        if((password) != nil){
-            border2.frame = CGRect(x: 0, y: password.frame.size.height - width, width:  password.frame.size.width, height: password.frame.size.height)
-            password.layer.addSublayer(border2)
-            password.layer.masksToBounds = true
-        }
-        ///////////////End textField//////////////////
-        
-    }
+    /////////////////////////////////////////////
     
+    ///////////////createUser//////////////////
     @IBAction func createUser(_ sender: Any) {
         let sv = AuthViewController.displaySpinner(onView: self.view)
         guard let usernameText = username.text,
@@ -129,72 +143,56 @@ class AuthViewController: UIViewController{
         Auth.auth().createUser(withEmail: usernameText, password: passwordText) { (user, error) in
             if let error = error{
                 debugPrint("#############")
-                debugPrint("∑∑∑∑∑∑∑∑∑∑∑∑∑")
+                debugPrint("createUser")
                 debugPrint("#############")
                 debugPrint(error.localizedDescription)
                 AuthViewController.removeSpinner(spinner: sv)
-                
-                let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
-                    NSLog("The \"OK\" alert occured.")
-                }))
-                
-                self.present((alert), animated: true, completion: nil)
+                self.displayAlert(userMessage: error.localizedDescription)
             }
             if !(error != nil){
-                debugPrint("#############")
-                debugPrint("////////////")
-                debugPrint("#############")
                 debugPrint(user)
-                self.switcMain()
+                self.switcPage(page: "Main")
             }
         }
-        
     }
+    //////////////////////////////////////////
     
+    /////////////////////ResetPassword/////////////////////
     @IBAction func ResetPassword(_ sender: Any) {
         let usernameText = username.text
         Auth.auth().sendPasswordReset(withEmail: usernameText!) { error in
             if let error = error{
                 debugPrint("#############")
-                debugPrint("∑∑∑∑∑∑∑∑∑∑∑∑∑")
+                debugPrint("ResetPassword")
                 debugPrint("#############")
                 debugPrint(error.localizedDescription)
+                self.displayAlert(userMessage: error.localizedDescription)
             }
-            debugPrint("#############")
-            debugPrint("////////////")
-            debugPrint("#############")
-            guard let appDeligate = UIApplication.shared.delegate as? AppDelegate else {return}
-            guard let main = UIStoryboard(name: "ForgotOK", bundle: Bundle.main).instantiateInitialViewController() else {return}
-            appDeligate.window?.rootViewController = main
+            if !(error != nil){
+                self.switcPage(page: "ForgotOK")
+            }
+            
         }
     }
+    //////////////////////////////////////////
     
+    ////////////////////ForgotPassword//////////////////////
     @IBAction func ForgotPassword(_ sender: Any) {
-        debugPrint("*************")
-        guard let appDeligate = UIApplication.shared.delegate as? AppDelegate else {return}
-        guard let main = UIStoryboard(name: "Forgot", bundle: Bundle.main).instantiateInitialViewController() else {return}
-        appDeligate.window?.rootViewController = main
+        self.switcPage(page: "Forgot")
     }
+    //////////////////////////////////////////
     
+    //////////////////SingUp////////////////////////
     @IBAction func SingUp(_ sender: Any) {
-        guard let appDeligate = UIApplication.shared.delegate as? AppDelegate else {return}
-        guard let main = UIStoryboard(name: "SingUp", bundle: Bundle.main).instantiateInitialViewController() else {return}
-        appDeligate.window?.rootViewController = main
+        self.switcPage(page: "SingUp")
     }
+    //////////////////////////////////////////
     
-    
+    ////////////////////////back button//////////////////
     @IBAction func back(_ sender: Any) {
-        guard let appDeligate = UIApplication.shared.delegate as? AppDelegate else {return}
-        guard let main = UIStoryboard(name: "Auth", bundle: Bundle.main).instantiateInitialViewController() else {return}
-        appDeligate.window?.rootViewController = main
+        self.switcPage(page: "Auth")
     }
+    //////////////////////////////////////////
     
-    private func switcMain(){
-        guard let appDeligate = UIApplication.shared.delegate as? AppDelegate else {return}
-        guard let main = UIStoryboard(name: "Main", bundle: Bundle.main).instantiateInitialViewController() else {return}
-        appDeligate.window?.rootViewController = main
-        
-    }
 }
 
