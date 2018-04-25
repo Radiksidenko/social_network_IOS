7191141e0231930fd8275638afe2f679513f4146
Radiksidenko <radik-sidehnko@ukr.net>
2018-04-25 15:34:52 +0300

    change in updating information about yourself

--- social_network/Scenes/additionalInfo/AInfoController.swift
+++ social_network/Scenes/additionalInfo/AInfoController.swift
@@ -13,6 +13,7 @@ import FirebaseDatabase
 
 class AInfoController: UIViewController {
     
+    
     @IBOutlet weak var changeFName: UITextField!
     @IBOutlet weak var changeLName: UITextField!
     @IBOutlet weak var changeNickname: UITextField!
@@ -25,8 +26,28 @@ class AInfoController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setInfo()
+        AInfoController.style(input: changeFName)
+        AInfoController.style(input: changeLName)
+        AInfoController.style(input: changeNickname)
+        AInfoController.style(input: gender)
+        AInfoController.style(input: phone)
+        
     }
-  
+    class func style(input :UITextField) {
+        let width = CGFloat(2.0)
+        
+        let style = CALayer()
+        style.borderColor = UIColor.darkGray.cgColor
+        style.borderWidth = width
+        style.frame = CGRect(x: 0, y: (input.frame.size.height) - width,
+                             width:  (input.frame.size.width),
+                             height: (input.frame.size.height))
+        input.layer.addSublayer(style)
+        input.layer.masksToBounds = true
+    }
+    
+    
     @IBAction func changeUserInfo(_ sender: Any) {
         let userID = Auth.auth().currentUser?.uid
         
@@ -41,8 +62,32 @@ class AInfoController: UIViewController {
             let main = UIStoryboard(name: "Main", bundle: Bundle.main).instantiateInitialViewController()
             appDeligate?.window?.rootViewController = main
         }
-        
-        
-    }
+     }
 
+    private func setInfo(){
+        let userID = Auth.auth().currentUser?.uid
+        ref.child("users").child(userID!).observeSingleEvent(of: .value, with: { (snapshot) in
+            DispatchQueue.main.async {
+                let value = snapshot.value as? NSDictionary
+                let username = value?["username"] as? String ?? ""
+                let userlname = value?["userlname"] as? String ?? ""
+                let usernickname = value?["usernickname"] as? String ?? ""
+                let userbio = value?["userbio"] as? String ?? ""
+                let genderVal = value?["gender"] as? String ?? ""
+                let phoneVal = value?["phone"] as? String ?? ""
+                
+                self.changeFName.text = username
+                self.changeLName.text = userlname
+                self.changeNickname.text = usernickname
+                self.changeBio.text = userbio
+                self.gender.text = genderVal
+                self.phone.text = phoneVal
+                
+                
+            }
+            
+        }) { (error) in
+            print(error.localizedDescription)
+        }
+    }
 }
