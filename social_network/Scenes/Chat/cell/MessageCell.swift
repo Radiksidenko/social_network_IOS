87e5919d408d488eb17245ccf5233457d1df4327
Radiksidenko <radik-sidehnko@ukr.net>
2018-06-03 15:56:05 +0300

    add color

---
+++ social_network/Scenes/Chat/cell/MessageCell.swift
@@ -0,0 +1,14 @@
+//
+//  MessageCell.swift
+//  social_network
+//
+//  Created by Radomyr Sidenko on 03.06.2018.
+//  Copyright © 2018 Radomyr Sidenko. All rights reserved.
+//
+
+import UIKit
+class MessageCell: UITableViewCell {
+    
+    @IBOutlet weak var message: UILabel!
+    
+}
